d3058c708be394ffd1a7712f0eb554c23054fd54
darkryder <sambhav13085@iiitd.ac.in>
2014-07-16 04:40:58 +0530

    UI improvements in supervisorportal. ref #2

--- supervisor/urls.py
+++ supervisor/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = patterns('',
 	url(r'^home/', views.home, name='supervisor_home'),
 	url(r'^logout/$', views._logout, name='supervisor_logout'),
-	url(r'^ongoing/(?P<skip>[0-9]+)/$', views.ongoing_projects, name='ongoing_projects'),
+	url(r'^ongoing/$', views.ongoing_projects, name='ongoing_projects'),
 	url(r'^unverified/$', views.unverified_projects, name='unverified_projects'),
 	url(r'^verify/(?P<project_id>[0-9]+)/$', views.verify_project, name='verify_project'),
 	url(r'^unverify/(?P<project_id>[0-9]+)/$', views.unverify_project, name='unverify_project'),
