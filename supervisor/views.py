0c2b6269283c928dfb9b0f6a49fa312239bbb55d
darkryder <sambhav13085@iiitd.ac.in>
2014-07-16 23:39:12 +0530

    add P.S to sent email

--- supervisor/views.py
+++ supervisor/views.py
@@ -335,7 +335,9 @@ def email_project(request, project_id):
 	if request.method == "POST":
 		form = EmailProjectForm(request.POST)
 		if form.is_valid():
-			EmailMessage("CW Project '%s' "%project.title, form.cleaned_data['body'], to=[form.cleaned_data['to']])
+			text = '\n\n'.join([form.cleaned_data['body'],
+			 "P.S. This mail is generated via the CW-portal. So for any further communication regarding the above mentioned issue(s), please reply to this mail, unless explicitly asked to create a new email thread, for proper redressal."])
+			EmailMessage("CW Project '%s' "%project.title, text, to=[form.cleaned_data['to']])
 			messages.success(request, "E-mail sent.")
 			return HttpResponseRedirect(reverse('super_viewproject', kwargs={'project_id':project.id}))
 	else:
