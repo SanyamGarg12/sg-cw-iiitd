acac9782887fec9128cb53c902e77b8c5debe672
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-03-20 19:31:52 +0530

    Updated requirements.txt, installation.sh, cw_database_generate.sql and settings.py.

--- installation.sh
+++ installation.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
-
+echo "Setting up virtual environment..."
 python3 -m venv env
 source env/bin/activate
 
-pip install requirements.txt
+echo "Installing pip3..."
+sudo apt install python3-pip
 
+echo "Installing requirements..."
+pip3 install requirements.txt
+
+echo "Installing dependencies..."
 sudo apt-get install memcached
+sudo apt install libmysqlclient-dev
+sudo apt install python3-dev
+sudo apt install build-essential
+sudo apt install libssl-dev
