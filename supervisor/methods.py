dcb8a6a5a9e12eb2edbeea67dda410bf72865784
Chirag Jain <jainchg@gmail.com>
2020-12-23 15:08:27 +0530

    update methods

--- supervisor/methods.py
+++ supervisor/methods.py
@@ -5,10 +5,10 @@ from supervisor.decorators import supervisor_logged_in
 @supervisor_logged_in
 def filtered_projects(request, **kwargs):
     queryset = None
-    if request.user.email == "sg@iiitd.ac.in":
-        queryset = Project.objects.filter(**kwargs)
-    elif request.user.email == "cw@iiitd.ac.in":
-        queryset = Project.objects.filter(category__name='CW', **kwargs)
-    elif request.user.email == "admin-btech@iiitd.ac.in":
+    if request.user.email == "selfgrowth@iiitd.ac.in":
         queryset = Project.objects.filter(category__name='SG', **kwargs)
+    elif request.user.email == "communitywork@iiitd.ac.in":
+        queryset = Project.objects.filter(category__name='CW', **kwargs)
+    else:
+        queryset = Project.objects.filter(**kwargs)
     return queryset
