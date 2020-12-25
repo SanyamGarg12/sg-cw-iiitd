1a693d01358e23ba576b712411a5bf9911cf8e0f
Chirag Jain <jainchg@gmail.com>
2020-12-25 14:39:09 +0530

    update 404

---
+++ studentportal/quotes.py
@@ -0,0 +1,11 @@
+quotes = [
+    ['A man travels the world over in search of what he needs and returns home to find it.', 'George A. Moore'],
+    ['Three things cannot be long hidden: the sun, the moon, and the truth.', 'Buddha'],
+    ['Honor thy error as a hidden intention.', 'Brian Eno'],
+    ['The cause is hidden; the effect is visible to all.', 'Ovid'],
+    ['Everything in this world has a hidden meaning.', 'Nikos Kazantzakis'],
+    ['Time will bring to light whatever is hidden; it will cover up and conceal what is now shining in splendor.',
+     'Horace'],
+    ['Nature is often hidden, sometimes overcome, seldom extinguished.', 'Francis Bacon'],
+    ['Hidden nature is secret God.', 'Sri Aurobindo']
+]
