b70a518a148492ae91431c3f98ea22e8f02e9b4e
Chirag Jain <jainchg@gmail.com>
2021-05-17 15:33:30 +0530

    add SG_USERS, CW_USERS

--- supervisor/methods.py
+++ supervisor/methods.py
@@ -1,13 +1,16 @@
 from studentportal.models import Project
 from supervisor.decorators import supervisor_logged_in
+from credentials import SG_USERS, CW_USERS, FULL_USERS
 
 
 @supervisor_logged_in
 def filtered_projects(request, **kwargs):
     queryset = None
-    if request.user.email == "selfgrowth@iiitd.ac.in":
+    if request.user.email in FULL_USERS:
+        queryset = Project.objects.filter(**kwargs)
+    elif request.user.email in SG_USERS:
         queryset = Project.objects.filter(category__name='SG', **kwargs)
-    elif request.user.email == "communitywork@iiitd.ac.in":
+    elif request.user.email in CW_USERS:
         queryset = Project.objects.filter(category__name='CW', **kwargs)
     else:
         queryset = Project.objects.filter(**kwargs)
