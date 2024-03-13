72ecfbd00c08d72a76ff16f0f7ea7fa187785abc
sanyam <sanyam22448@iiitd.ac.in>
2024-03-13 10:48:05 +0530

    as url was deprecated, so all instances have now been changed to path in all urls.py files

--- CW_Portal/urls.py
+++ CW_Portal/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 import studentportal
@@ -18,14 +19,14 @@ handler404 = 'studentportal.views.handle404_LnF'
 handler500 = 'studentportal.views.handle404_LnF'
 
 urlpatterns = [
-                  url(r'^$', index, name='index'),
-                  url('', include('social_django.urls', namespace='social')),
-                  url(r'^first_login/$', first_login),
-                  url(r'^student/', include('studentportal.urls')),
-                  url(r'^supervisor/', include('supervisor.urls')),
-                  url(r'^admin/', admin.site.urls),
-                  url(r'^accounts/', include('allauth.urls')),
-                  url(r'^logout/$', studentportal.views._logout, name='logout'),
+                  path(r'^$', index, name='index'),
+                  path('', include('social_django.urls', namespace='social')),
+                  path(r'^first_login/$', first_login),
+                  path(r'^student/', include('studentportal.urls')),
+                  path(r'^supervisor/', include('supervisor.urls')),
+                  path(r'^admin/', admin.site.urls),
+                  path(r'^accounts/', include('allauth.urls')),
+                  path(r'^logout/$', studentportal.views._logout, name='logout'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
--- studentportal/urls.py
+++ studentportal/urls.py
@@ -1,50 +1,49 @@
-from django.conf.urls import url
-
+from django.urls import path
 from . import views
 
 urlpatterns = (
     # basic
-    url(r'^home/', views.home, name='studenthome'),
-    url(r'^$', views.index, name='index'),
-    url(r'^profile/$', views.profile, name='studentprofile'),
+    path(r'^home/', views.home, name='studenthome'),
+    path(r'^$', views.index, name='index'),
+    path(r'^profile/$', views.profile, name='studentprofile'),
 
     # projects and documents
-    url(r'^add/$', views.addproject, name='addproject'),
-    url(r'^project/(?P<project_id>[0-9]+)/edit/$', views.editproject, name='editproject'),
-    url(r'^project/(?P<project_id>[0-9]+)/upload/$', views._upload, name='upload_document'),
-    url(r'^project/(?P<project_id>[0-9]+)/$', views.viewproject, name='viewproject'),
-    url(r'^project/(?P<project_id>[0-9]+)/submit/$', views.submitproject, name='submitproject'),
-    url(r'^project/(?P<project_id>[0-9]+)/NGO/$', views.view_project_NGO, name='view_project_NGO'),
-    url(r'^link_ngo_project/(?P<NGO_id>[0-9]+)/(?P<project_id>[0-9]+)/$',
+    path(r'^add/$', views.addproject, name='addproject'),
+    path(r'^project/(?P<project_id>[0-9]+)/edit/$', views.editproject, name='editproject'),
+    path(r'^project/(?P<project_id>[0-9]+)/upload/$', views._upload, name='upload_document'),
+    path(r'^project/(?P<project_id>[0-9]+)/$', views.viewproject, name='viewproject'),
+    path(r'^project/(?P<project_id>[0-9]+)/submit/$', views.submitproject, name='submitproject'),
+    path(r'^project/(?P<project_id>[0-9]+)/NGO/$', views.view_project_NGO, name='view_project_NGO'),
+    path(r'^link_ngo_project/(?P<NGO_id>[0-9]+)/(?P<project_id>[0-9]+)/$',
         views.link_NGO_project, name='link_NGO_project'),
-    url(r'^unlink_ngo_project/(?P<project_id>[0-9]+)/$', views.unlink_NGO_project,
+    path(r'^unlink_ngo_project/(?P<project_id>[0-9]+)/$', views.unlink_NGO_project,
         name='unlink_ngo_project'),
-    url(r'^download/(?P<document_id>[0-9]+)/$', views.download, name='download_document'),
-    url(r'^delete/(?P<project_id>[0-9]+)/$', views.delete_project, name='delete_project'),
-    url(r'^delete/document/(?P<document_id>[0-9]+)/$', views.delete_document, name='delete_document'),
-    url(r'^feedback/(?P<project_id>[0-9]+)/$', views.feedback, name='feedback'),
+    path(r'^download/(?P<document_id>[0-9]+)/$', views.download, name='download_document'),
+    path(r'^delete/(?P<project_id>[0-9]+)/$', views.delete_project, name='delete_project'),
+    path(r'^delete/document/(?P<document_id>[0-9]+)/$', views.delete_document, name='delete_document'),
+    path(r'^feedback/(?P<project_id>[0-9]+)/$', views.feedback, name='feedback'),
 
     # open for all
-    url(r'^news/$', views.view_news, name='student_view_news_all'),
-    url(r'^news/(?P<news_id>[0-9]*)/$', views.view_news, name='student_view_news'),
-    url(r'^guidelines/$', views.guidelines, name='guidelines'),
-    url(r'^bugs/$', views.bugs, name='bugs'),
+    path(r'^news/$', views.view_news, name='student_view_news_all'),
+    path(r'^news/(?P<news_id>[0-9]*)/$', views.view_news, name='student_view_news'),
+    path(r'^guidelines/$', views.guidelines, name='guidelines'),
+    path(r'^bugs/$', views.bugs, name='bugs'),
 
-    url(r'^update_batch_student/$', views.update_batch_student, name='update_batch_student'),
+    path(r'^update_batch_student/$', views.update_batch_student, name='update_batch_student'),
 
-    url(r'^all_ngo/$', views.all_NGOs, name='all_NGO'),
-    url(r'^suggest_ngo/$', views.suggest_NGO, name='suggest_ngo'),
+    path(r'^all_ngo/$', views.all_NGOs, name='all_NGO'),
+    path(r'^suggest_ngo/$', views.suggest_NGO, name='suggest_ngo'),
 
-    url(r'^allprojects/$',
+    path(r'^allprojects/$',
         views.all_projects_open_to_public_year_select, name='all_projects_open_to_public_year_select'),
-    url(r'^allprojects/(?P<year>[0-9]+)/$', views.all_projects_open_to_public,
+    path(r'^allprojects/(?P<year>[0-9]+)/$', views.all_projects_open_to_public,
         name='all_projects_open_to_public'),
 
     # example projects
-    url(r'^examples/all/$', views.all_examples, name='student_all_examples'),
-    url(r'^examples/(?P<example_id>[0-9]+)/$', views.view_example, name='view_example'),
-    url(r'^like/(?P<example_id>[0-9]+)/$', views.like_project, name='like_project'),
-    url(r'^unlike/(?P<example_id>[0-9]+)/$', views.unlike_project, name='unlike_project'),
-    url(r'^comment/(?P<example_id>[0-9]+)/$', views.add_comment, name='add_comment'),
-    url(r'^comment/delete/(?P<comment_id>[0-9]+)/$', views.delete_comment, name='delete_comment'),
+    path(r'^examples/all/$', views.all_examples, name='student_all_examples'),
+    path(r'^examples/(?P<example_id>[0-9]+)/$', views.view_example, name='view_example'),
+    path(r'^like/(?P<example_id>[0-9]+)/$', views.like_project, name='like_project'),
+    path(r'^unlike/(?P<example_id>[0-9]+)/$', views.unlike_project, name='unlike_project'),
+    path(r'^comment/(?P<example_id>[0-9]+)/$', views.add_comment, name='add_comment'),
+    path(r'^comment/delete/(?P<comment_id>[0-9]+)/$', views.delete_comment, name='delete_comment'),
 )
